eced3640a523fa5c31bd6ece95374199c661bd98
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-07 09:51:03 -0300

    girando a imagem, adcionando opçoes de menu, e atualizando o codigo js

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,40 @@
 window.onload = function(){
 
-    const intro=document.getElementById("inicio");
+    const intro = document.getElementById("inicio");
+    const content = document.getElementById("content");
 
     setTimeout(()=>{
-        intro.classList.add("efeito-out")
+        intro.classList.add("efeito-out");
+
         setTimeout(()=>{
-            intro.style.display="none";
-            content.classList.remove("hidden");
-            content.classList.add("efeito-in");
-        },1000)
-    },3000)
+            intro.style.display = "none";
+
+            if(content){
+                content.classList.remove("hidden");
+                content.classList.add("efeito-in");
+            }
+
+        },1000);
+
+    },3000);
 }
 
 function trocar(cor){
     document.body.style.background = cor;
 }
+
+// MENU HAMBURGUER
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const menuIcone = document.getElementById("menu-icone");
+    const navMenu = document.querySelector("nav");
+
+    if(menuIcone && navMenu){
+
+        menuIcone.onclick = ()=>{
+
+            navMenu.classList.toggle('active');
+            menuIcone.classList.toggle('open');
+        }
+    }
+});
